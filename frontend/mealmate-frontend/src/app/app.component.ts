fa5cdcf287c53f9e411ca6f222afb827e1fce39c
fe-vm <eddieromerobuis@gmail.com>
2025-12-17 18:07:12 -0500

    Front end login shows

--- frontend/mealmate-frontend/src/app/app.component.ts
+++ frontend/mealmate-frontend/src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ApiService } from './services/api.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>MealMate Frontend</h1>
-    <button (click)="checkHealth()">Test API Health</button>
-    <pre>{{ result | json }}</pre>
-  `
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  result: any;
-
-  constructor(private api: ApiService) {}
-
-  checkHealth() {
-    this.api.health().subscribe({
-      next: res => this.result = res,
-      error: err => this.result = err
-    });
-  }
-}
+export class AppComponent {}
